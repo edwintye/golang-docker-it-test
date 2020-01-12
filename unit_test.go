e0d8a01983325b6de15ee1544b36fa815165e2fa
Edwin Tye <edwin.tye@gmail.com>
2020-01-12 23:04:14 +0000

    Adding a simple main

---
+++ unit_test.go
@@ -0,0 +1,31 @@
+// +build unit
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	exitCode := m.Run()
+	os.Exit(exitCode)
+	rc.Close()
+}
+
+func TestHello(t *testing.T) {
+	s := "World"
+	t.Run("Testing hello", func(t *testing.T) {
+		if hello(s) != fmt.Sprintf("Hello %s", s) {
+			t.Error("Didn't manage to say hello")
+		}
+	})
+
+	t.Run("Testing helloCache without a redis client", func(t *testing.T) {
+		if !strings.HasPrefix(helloCache(s), fmt.Sprintf("Hello %s", s)) {
+			t.Error("Didn't manage to say helloCache to the world")
+		}
+	})
+}
